4c8be05258dbfc4042f88682ca18c2830be71791
Chris F <chrisfirmani@hotmail.com>
2022-09-19 21:51:45 -0400

    Added black to format

--- src/main.py
+++ src/main.py
@@ -2,8 +2,10 @@
 Main entry point
 """
 
+
 def main():
     return "Hello World!"
+
 
 if __name__ == "__main__":
     print(main())
--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,8 @@ Testing main.py
 
 from src import main
 
+
 def test_main():
-    """ Test to see return valid"""
+    """Test to see return valid"""
     ret = main.main()
     assert ret == "Hello World!"
