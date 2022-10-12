af84a3e25cb3377852787b3b64625a258bc9a6e0
ctfirman <chrisfirmani@hotmail.com>
2022-10-11 23:19:51 -0400

    First implementation of crack detect

---
+++ src/crack_detect/crack_detect.py
@@ -0,0 +1,75 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def crack_detect_method_1(img_src: str) -> None:
+    """
+    Algo from https://github.com/shomnathsomu/crack-detection-opencv
+    1. read image
+    2. Gray scale and average
+    3. log transform
+    4. Image smoothing: bilateral filter
+    5. Image segmentation Techniques
+        - Canny edge detection
+        - Morphological closing operator
+        - Feature extraction
+    """
+    print(img_src)
+
+    src = cv2.imread(img_src)
+    # cv2.imshow("input", src)
+
+    gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    # cv2.imshow("gray", gray)
+
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    cv2.imshow("blur", blur)
+
+    # Apply logarithmic transform
+    img_log = (np.log(blur + 1) / (np.log(1 + np.max(blur)))) * 255
+
+    # Specify the data type
+    img_log = np.array(img_log, dtype=np.uint8)
+    cv2.imshow("img_log", img_log)
+
+    # Image smoothing: bilateral filter
+    bilateral = cv2.bilateralFilter(img_log, 5, 25, 25)
+    cv2.imshow("bilateral", bilateral)
+
+    # ret, binary = cv2.threshold(
+    #     bilateral, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU
+    # )
+    # cv2.imshow("binary", binary)
+
+    # Canny Edge Detection
+    # max value will be half of max - min from bilateral
+
+    # TODO: Figure out why bilateral image won't work
+    edges = cv2.Canny(gray, 100, 200)
+    cv2.imshow("edges", edges)
+
+    # Morphological Closing Operator
+    kernel = np.ones((5, 5), np.uint8)
+    closing = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
+    # cv2.imshow("closing", closing)
+
+    # Create feature detecting method
+    # sift = cv2.xfeatures2d.SIFT_create()
+    # surf = cv2.xfeatures2d.SURF_create()
+    orb = cv2.ORB_create(nfeatures=1500)
+
+    # Make featured Image
+    keypoints, descriptors = orb.detectAndCompute(closing, None)
+    featuredImg = cv2.drawKeypoints(closing, keypoints, None)
+    # cv2.imshow("result", featuredImg)
+
+    # Use plot to show original and output image
+    plt.subplot(121), plt.imshow(src)
+    plt.title("Original"), plt.xticks([]), plt.yticks([])
+    plt.subplot(122), plt.imshow(featuredImg, cmap="gray")
+    plt.title("Output Image"), plt.xticks([]), plt.yticks([])
+    plt.show()
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
--- src/main.py
+++ src/main.py
@@ -1,6 +1,20 @@
+import sys
+import os
+
+from crack_detect.crack_detect import crack_detect_method_1
+
+
 def main():
     return "Hello World!"
 
 
 if __name__ == "__main__":
     print(main())
+
+    img_path = sys.argv
+    if len(img_path) > 1:
+        img_path = os.path.abspath(f"test-images/concrete/{img_path[1]}")
+    else:
+        img_path = os.path.abspath(f"test-images/concrete/00019.jpg")
+
+    crack_detect_method_1(img_src=img_path)
