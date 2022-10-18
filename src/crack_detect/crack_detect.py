20a03d1c11ce4c2285537d0bfa01ab9e90f2cb83
ctfirman <chrisfirmani@hotmail.com>
2022-10-17 21:57:03 -0400

    Removed unused code

--- src/crack_detect/crack_detect.py
+++ src/crack_detect/crack_detect.py
@@ -1,11 +1,13 @@
 import os
+from typing import Tuple
 
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
 
-def crack_detect_method_1(img_src: str) -> None:
+def crack_detect_method_1(
+    img_src: str, save_img: bool = False
+) -> Tuple[cv2.Mat, cv2.Mat]:
     """
     Algo from https://github.com/shomnathsomu/crack-detection-opencv
     1. read image
@@ -20,42 +22,24 @@ def crack_detect_method_1(img_src: str) -> None:
     print(img_src)
 
     src = cv2.imread(img_src)
-    # cv2.imshow("input", src)
-
     gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-    # cv2.imshow("gray", gray)
-
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    # cv2.imshow("blur", blur)
 
     # Apply logarithmic transform
     img_log = (np.log(blur + 1) / (np.log(1 + np.max(blur)))) * 255
 
     # Specify the data type
     img_log = np.array(img_log, dtype=np.uint8)
-    # cv2.imshow("img_log", img_log)
 
     # Image smoothing: bilateral filter
     bilateral = cv2.bilateralFilter(img_log, 5, 10, 10)
-    # cv2.imshow("bilateral", bilateral)
 
-    # ret, binary = cv2.threshold(
-    #     bilateral, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU
-    # )
-    # cv2.imshow("binary", binary)
-
-    # Canny Edge Detection
-    # max value will be half of max - min from bilateral
-
-    # TODO: Figure out why bilateral image won't work
-    # edges = cv2.Canny(blur, 100, 150)
+    # Run Canny Edge Detector
     edges = cv2.Canny(bilateral, 25, 50)
-    # cv2.imshow("edges", edges)
 
     # Morphological Closing Operator
     kernel = np.ones((5, 5), np.uint8)
     closing = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
-    # cv2.imshow("closing", closing)
 
     # Create feature detecting method
     # sift = cv2.xfeatures2d.SIFT_create()
@@ -65,12 +49,14 @@ def crack_detect_method_1(img_src: str) -> None:
     # Make featured Image
     keypoints, descriptors = orb.detectAndCompute(closing, None)
     result = cv2.drawKeypoints(closing, keypoints, None)
-    # cv2.imshow("result", result)
 
-    img_file_name = os.path.basename(img_src)
-    final_img_path = os.path.abspath(f"test-images/concrete/completed/{img_file_name}")
-    # cv2.imwrite(final_img_path, result)
-    # cv2.waitKey(0)
+    if save_img:
+        img_file_name = os.path.basename(img_src)
+        final_img_path = os.path.abspath(
+            f"test-images/concrete/completed/{img_file_name}"
+        )
+        cv2.imwrite(final_img_path, result)
+
     cv2.destroyAllWindows()
 
     return src, result
--- src/main.py
+++ src/main.py
@@ -13,26 +13,19 @@ def main():
 if __name__ == "__main__":
     print(main())
 
-    img_path = sys.argv
-    if len(img_path) > 1:
-        img_paths = [os.path.abspath(f"test-images/concrete/{img_path[1]}")]
-    else:
-        img_paths = [
-            os.path.abspath(f"test-images/concrete/original/positive0{i}.jpg")
-            for i in range(1, 4)
-        ]
-        img_paths.append(
-            os.path.abspath("test-images/concrete/original/negative01.jpg")
-        )
-        img_paths.append(
-            os.path.abspath("test-images/concrete/original/negative02.jpg")
-        )
+    save_img = "save-img" in sys.argv
+    img_paths = [
+        os.path.abspath(f"test-images/concrete/original/positive0{i}.jpg")
+        for i in range(1, 4)
+    ]
+    img_paths.append(os.path.abspath("test-images/concrete/original/negative01.jpg"))
+    img_paths.append(os.path.abspath("test-images/concrete/original/negative02.jpg"))
 
     fig, axes = plt.subplots(5, 2)
     fig.suptitle("Original Vs Crack Detection")
 
     for count, img in enumerate(img_paths):
-        src, result = crack_detect_method_1(img_src=img)
+        src, result = crack_detect_method_1(img_src=img, save_img=save_img)
 
         axes[count, 0].imshow(src)
         axes[count, 0].set_xticks([]), axes[count, 0].set_yticks([])
