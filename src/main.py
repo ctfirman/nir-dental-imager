2885e3b8e7c4c9430c3c182f645a948fcc2b0a38
Chris F <chrisfirmani@hotmail.com>
2023-03-05 17:01:28 -0500

    added code to save ml data

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import numpy as np
 
 # import matplotlib.pyplot as plt
 
@@ -7,6 +8,7 @@ import os
 # from utils.camera import main_video_stream
 
 from utils.gui import run_gui
+from utils.database import nmlDB
 
 
 def main():
@@ -43,4 +45,32 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+
+    db = nmlDB("nml.db")
+
+    all_classifier_list = []
+    all_data_list = []
+
+    all_crack = db.get_all_ml_data("CRACK")
+    for entry in all_crack:
+        after_img = np.frombuffer(entry.img, dtype=np.uint8)
+        all_classifier_list.append(entry.classifier)
+        all_data_list.append(after_img)
+
+    no_crack = db.get_all_ml_data("NO_CRACK")
+    for entry in no_crack:
+        after_img = np.frombuffer(entry.img, dtype=np.uint8)
+        all_classifier_list.append(entry.classifier)
+        all_data_list.append(after_img)
+
+    np.save("test_data.npy", all_data_list)
+    np.save("test_classifiers.npy", all_classifier_list)
+
+    data = np.load("test_data.npy")
+    classifiers = np.load("test_classifiers.npy")
+    print(f"data shape = {data.shape}")
+    print(f"data example = {data}")
+
+    print(f"classifiers shape = {classifiers.shape}")
+    print(f"classifiers example = {classifiers}")
